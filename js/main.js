cf9be0a80638d43ccb8514fb7d86152372b09874
Ronney Vargas <ronneyvargas@gmail.com>
2018-09-03 12:44:01 -0500

    type: doc
    Made change to image.alt.

--- js/main.js
+++ js/main.js
@@ -163,7 +163,7 @@ createRestaurantHTML = (restaurant) => {
     const image = document.createElement('img');
     image.className = 'restaurant-img';
     image.src = DBHelper.imageUrlForRestaurant(restaurant);
-    image.setAttribute("alt", restaurant.name);
+    image.alt = restaurant.name;
     li.append(image);
 
     const name = document.createElement('h1');
